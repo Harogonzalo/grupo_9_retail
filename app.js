65ba024567cee450a3cfe5bc11ae93c8c6a24fc9
sombrerodiego <sombrerodiego@gmail.com>
2022-06-03 00:06:21 -0300

    Agregue Error

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'))
 ;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+    res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('listening on http://localhost:3000');
+app.listen(4000, () => {
+    console.log('listening on http://localhost:4000');
 });
