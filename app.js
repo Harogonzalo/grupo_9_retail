dac335a148c799ca5d5923edf1388bc987019021
sombrerodiego <sombrerodiego@gmail.com>
2022-06-02 23:33:53 -0300

    index y app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, 'public'))
-);
+app.use(express.static('public'))
+;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 app.listen(3000, () => {
