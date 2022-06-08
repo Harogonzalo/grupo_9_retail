9abf5521767b5aae05ac05b91c62325bd9a58852
sombrerodiego <sombrerodiego@gmail.com>
2022-06-08 15:52:33 -0300

    Cambie el app para los hipervinculos

--- app.js
+++ app.js
@@ -4,24 +4,49 @@ const path = require('path');
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'));
-//app.use(express.static('views'));
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views'));
-});
+});*/
 
 
 /*const loginRoutes = require("./views/login.html");
 
 app.use('/login', loginRoutes);*/
 
+// Error 404
+//app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, './views/404.html'));
+//});
+
+// Login
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+// productCart
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'));
+});
+
+// productDetail
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
+// register
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
 
 
+
+
+/*
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/404.html'));
 });
@@ -37,7 +62,7 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
 });
-
+*/
 
 
 app.listen(4000, () => {
