b0d40d3e5cf27a05e5f30a5077f6c304cda92252
aaverbaj <aaverbaj@gmail.com>
2022-06-22 20:56:54 -0300

    repare el app.js

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 4000;
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || 'localhost';
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-// Error 404
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './views/404.html'));
-// });
-
-// Login
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
 
-// productCart
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productCart.html'));
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
 
-// productDetail
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+app.get('/productCart', (req,res)=>{
+    res.sendFile(__dirname + '/views/productcart.html');
 });
 
-// register
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
-});
-
-/*app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
-});*/
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Servidor corriendo en el pueretpo 3000');
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://${HOST}:${PORT}/`);
 });
