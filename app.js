63e5f59ce05457a9499d6c564cca30ba12959954
harogonzalo9@gmail.com <harogonzalo9@gmail.com>
2022-06-24 13:04:28 -0300

    cmabios

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
 app.get('/', (req,res)=>{
