3a2be5f61e34cd073938e502c2dea004500b7453
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2022-07-07 20:12:43 -0300

    acomode css productDetail y cree weekly.md

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,3 +5,84 @@ const mainController = {
 }
 
 module.exports = mainController;
+
+
+const listProducts =
+[
+    {
+     "id": 0,
+     "name": "SmartTv Samsung 43",
+     "description": "bla bnla bla",
+     "imagen": "/img/img-tv-samsung-smart.jpg",
+     "price": 60200,
+     "discount": 5,
+     "category": "ofertas"
+    },
+    {
+     "id": 1,
+     "name": "Horno Electrico",
+     "description": "bla bl bla",
+     "imagen": "/img/horno-electrico-.jpg",
+     "price": 34200,
+     "discount": 10,
+     "category": "novedades"
+    },
+    {
+     "id": 2,
+     "name": "Cafetera Moulinex",
+     "description": "Juego Red Dead Redemption II para xbox, Rockstar Games",
+     "imagen": "imagen-1616099612014.jpg",
+     "price": 30500,
+     "discount": 40,
+     "category": "ofertas"
+    },
+    {
+     "id": 3,
+     "name": "Pava Electrica Atma",
+     "description": "PAva bla bla bla",
+     "imagen": "/img/pava-atma.jpg",
+     "price": 6770,
+     "discount": 15,
+     "category": "novedades"
+    },
+    {
+     "id": 4,
+     "name": "Notebook Lenovo",
+     "description": "Intel Core i5 10210U 12GB de RAM 512GB SSD, Intel UHD Graphics 620 1920x1080px",
+     "price": "$154999",
+     "discount": "15",
+     "interes": "novedades",
+     "imagen": "/img/notebook.jpg"
+    },
+    {
+     "id": 5,
+     "name": "Samsung Galaxy A33",
+     "description": " 5g 128 Gb Awesome Black 6 Gb Ram ",
+     "price": "$65000",
+     "discount": "",
+     "interes": "novedades",
+     "imagen": "/img/telefono.jpg"
+    },
+    {
+     "id": 6,
+     "name": "Heladera Patrick",
+     "description": "HPK141M10 silver con freezer 355L 220V",
+     "imagen": "imagen-1617485554035.jpg",
+     "price": 102000,
+     "discount": 25,
+     "category": "ofertas"
+    }
+   ]
+   
+   const mainProducts = {
+       index: (req, res) => {
+           res.render("index", { products: listProducts });
+       },
+       detalle: (req, res) => {
+           const id = req.params.id;
+           const anyPrducts = listProducts.find(anyPrducts => anyPrducts.id == id);
+           res.render("detalleProducts", { anyPrducts });
+       }
+   }
+   
+   module.exports = mainProducts;
---
+++ weekly.md
@@ -0,0 +1,9 @@
+# 6-7-2022
+
+- Se separaron todas las rutas del archivo app.js hacia la carpeta routes
+- Partial de head creado.
+- Partial de footer creado
+- Partial de footer agregado a todas las vistas
+- Se efectuó la reunión semanal para programar y distribuir todas las tareas relacionadas con el sprint 3
+- Limpia de código
+- Se reestructuró el directorio de vistas separándo las vistas principales, las vistas de productos y las vistas de usuario.
