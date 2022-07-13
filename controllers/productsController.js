4b7cb33e25ba07a5af0348bd3e4db43f26792cc1
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2022-07-13 19:30:15 -0300

    partial para articulos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,79 @@
-const productsController = {
-    productDetail: (req, res) => {
-        res.render('products/productDetail');
-    },
+const productList = [
+    {
+    id: 1,
+    name: "Horno Electrico",
+    description: "bla bl bla",
+    imagen: "/img/horno-electrico-.jpg",
+    price: 34200,
+    discount: 10,
+    category: "novedades",
+  },
+  {
+    id: 2,
+    name: "Cafetera Moulinex",
+    description: "Juego Red Dead Redemption II para xbox, Rockstar Games",
+    imagen: "imagen-1616099612014.jpg",
+    price: 30500,
+    discount: 40,
+    category: "ofertas",
+  },
+  {
+    id: 3,
+    name: "Pava Electrica Atma",
+    description: "PAva bla bla bla",
+    imagen: "/img/pava-atma.jpg",
+    price: 6770,
+    discount: 15,
+    category: "novedades",
+  },
+  {
+    id: 4,
+    name: "Notebook Lenovo",
+    description:
+      "Intel Core i5 10210U 12GB de RAM 512GB SSD, Intel UHD Graphics 620 1920x1080px",
+    price: "$154999",
+    discount: "15",
+    interes: "novedades",
+    imagen: "/img/notebook.jpg",
+  },
+  {
+    id: 5,
+    name: "Samsung Galaxy A33",
+    description: " 5g 128 Gb Awesome Black 6 Gb Ram ",
+    price: "$65000",
+    discount: "",
+    interes: "novedades",
+    imagen: "/img/telefono.jpg",
+  },
+  {
+    id: 6,
+    name: "Heladera Patrick",
+    description: "HPK141M10 silver con freezer 355L 220V",
+    imagen: "imagen-1617485554035.jpg",
+    price: 102000,
+    discount: 25,
+    category: "ofertas",
+  },
+  {
+    id: 7,
+    name: "SmartTv Samsung 43",
+    description: "bla bnla bla",
+    imagen: "/img/img-tv-samsung-smart.jpg",
+    price: 60200,
+    discount: 5,
+    category: "ofertas",
+  },
+];
 
-    productCart: (req, res) => {
-         res.render('products/productCart');
-    },
-}
+const productsController = {
+  productDetail: (req, res) => {
+    res.render("products/productDetail");
+  },
+
+  productCart: (req, res) => {
+    res.render("products/productCart");
+  },
+};
 
 module.exports = productsController;
+
