262517400e9a59a3e7ef7de1d546cfba15a33987
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2022-07-14 18:56:11 -0300

    productsRoutes y ProductsController

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -64,15 +64,101 @@ const listProducts = [
   },
 ];
 
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+const productListPath = path.resolve(__dirname, '../data/products.json');
+const productList = JSON.parse(fs.readFileSync(productListPath, 'utf8'));
+
+// const productsController = {
+//   productDetail: (req, res) => {
+//     res.render("products/productDetail", {productsDetail: listProducts});
+//   },
+
+//   productCart: (req, res) => {
+//     res.render("products/productCart", {productsCart: listProducts});
+//   },
+// };
+
 const productsController = {
   productDetail: (req, res) => {
-    res.render("products/productDetail", {productsDetail: listProducts});
+    res.render("products/productDetail", {productsDetail: productList});
+       },
+    
+       productCart: (req, res) => {
+         res.render("products/productCart", {productsCart: productList});
+       },
+  
+  
+  getAllProducts: (req, res) => {
+      res.render('index', {
+          products: productList
+      });
   },
+  getProductById: (req, res) => {
+      let id = req.params.id;
 
-  productCart: (req, res) => {
-    res.render("products/productCart", {productsCart: listProducts});
+      res.send('Get product by id: ' + id);
   },
-};
+  createProduct: (req, res) => {
+      res.render('products/createProducts');
+  },
+  storeProduct: (req, res) => {
+      let product = req.body;
+
+      product.id = uuidv4();
+
+      productList.push(product);
+
+      fs.writeFileSync(productListPath, JSON.stringify(productList, null, 2));
+
+      res.redirect('/products');
+  },
+  editProduct: (req, res) => {
+      let id = req.params.id;
+      let product = productList.find(product => product.id == id);
+
+      res.render('products/editProduct', {
+          products: product
+      });
+  },
+  updateProduct: (req, res) => {
+      let id = req.params.id;
+      let newProduct = req.body;
+
+      newProduct.id = id;
+
+      for (let index = 0; index < productList.length; index++) {
+          const element = productList[index];
+          if (element.id == id) {
+              productList[index] = newProduct;
+          }
+      }
+
+      fs.writeFileSync(productListPath, JSON.stringify(productList, null, 2));
+
+      res.redirect('/products');
+  },
+  deleteProduct: (req, res) => {
+      let id = req.params.id;
+      console.log("deleteProduct", id);
+      for (let index = 0; index < productList.length; index++) {
+          const element = productList[index];
+          if (element.id == id) {
+              productList.splice(index, 1);
+          }
+      }
+
+      fs.writeFileSync(productListPath, JSON.stringify(productList, null, 2));
+
+      res.redirect('/products');
+  }
+}
+
+
+
+
 
 module.exports = productsController;
 
