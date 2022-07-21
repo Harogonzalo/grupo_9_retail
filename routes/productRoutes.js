9942bc9a652e96495a4da46cb875da53d701e362
harogonzalo9@gmail.com <harogonzalo9@gmail.com>
2022-07-21 19:16:28 -0300

    Levantando vender

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,13 +6,17 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 
-router.post('/storeProduct', productsController.storeProduct);
+//router.post('/storeProduct', productsController.storeProduct);
 
 
 //RUTAS A VISTAS
 
 router.get("/productCart", productsController.productCart);
 router.get("/productDetail/:id", productsController.productDetail);
+router.get("/products/:id/edit", productsController.editProduct);
+router.get("/createProduct", productsController.createProduct);
+router.get("/products/:id", productsController.getProductById);
+/*
 
 //RUTAS CRUD
 // RUTA Listado de productos
@@ -21,14 +25,14 @@ router.get("/products", productsController.getAllProducts);
 // RUTA Formulario de creación de productos
 router.get("/createProduct", productsController.createProduct);
 //router.post('/', upload.single('image'), productsController.storeProduct);
-router.post("/", validate, upload.array("image", 3),productsController.storeProduct);
+//router.post("/", validate, upload.array("image", 3),productsController.storeProduct);
 
 // RUTA Detalle de un producto particular
 router.get("/products/:id", productsController.getProductById);
 
 //  /products (POST)
 // RUTA Acción de creación (a donde se envía el formulario)
-router.post('/storeProduct', productsController.storeProduct);
+//router.post('/storeProduct', productsController.storeProduct);
 
 //RUTA Formulario de edición de productos
 router.get("/products/:id/edit", productsController.editProduct);
@@ -38,5 +42,5 @@ router.put("/products/:id", productsController.updateProduct);
 
  //RUTA Acción de borrado
 router.delete("/products/:id", productsController.deleteProduct);
-
+*/
 module.exports = router;
