1cdd146066f93e6fa1e9d48940ef15d567e08ace
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2022-07-22 13:09:46 -0300

    agregue middlewares

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,41 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-//const upload = require('../middlewares/multer');
-
 const productsController = require('../controllers/productsController');
-
-
-//router.post('/storeProduct', productsController.storeProduct);
-
+const upload = require('../middlewares/multer');
+const validateProduct = require('../middlewares/productValidate');
 
 //RUTAS A VISTAS
 
 router.get("/productCart", productsController.productCart);
 router.get("/productDetail/:id", productsController.productDetail);
+//RUTA Formulario de edición de productos
 router.get("/products/:id/edit", productsController.editProduct);
-router.get("/createProduct", productsController.createProduct);
-router.get("/products/:id", productsController.getProductById);
-/*
-
-//RUTAS CRUD
-// RUTA Listado de productos
-router.get("/products", productsController.getAllProducts);
-
 // RUTA Formulario de creación de productos
 router.get("/createProduct", productsController.createProduct);
-//router.post('/', upload.single('image'), productsController.storeProduct);
-//router.post("/", validate, upload.array("image", 3),productsController.storeProduct);
+router.post('/createProduct', upload.array ('images', 5), validateProduct, productsController.updateProduct);
 
 // RUTA Detalle de un producto particular
 router.get("/products/:id", productsController.getProductById);
-
-//  /products (POST)
-// RUTA Acción de creación (a donde se envía el formulario)
-//router.post('/storeProduct', productsController.storeProduct);
-
-//RUTA Formulario de edición de productos
-router.get("/products/:id/edit", productsController.editProduct);
+/*
+// RUTA Listado de productos
+router.get("/products", productsController.getAllProducts);
 
  //RUTA Acción de edición (a donde se envía el formulario)
 router.put("/products/:id", productsController.updateProduct);
